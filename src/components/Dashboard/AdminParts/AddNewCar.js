90cc0b1ba343fef133914f5a7374599ededa4f9c
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-02-07 21:42:24 +0300

    delete icon to delete said images

--- src/components/Dashboard/AdminParts/AddNewCar.js
+++ src/components/Dashboard/AdminParts/AddNewCar.js
@@ -101,7 +101,7 @@ useEffect(()=>{
   const handleValueChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
-  const twoMinutes = 6* 60 * 1000;//waiting time to upload
+  const twoMinutes = 2* 60 * 1000;//waiting time to upload
   // add new car in database
   const handleSubmit = (event) => {
      
